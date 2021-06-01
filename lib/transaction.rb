3693d9fc636789a81a7908ce5449bc62336aa440
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-06-01 17:06:51 +0100

    Current date initialized with transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,7 +1,8 @@
 class Transaction
-  attr_reader :amount
+  attr_reader :amount, :date
 
   def initialize(amount)
     @amount = amount
+    @date = Time.now.strftime("%d/%m/%Y")
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,7 +1,12 @@
 RSpec.describe Transaction do
   let(:subject) { described_class.new(amount = 100) }
+  let(:current_date) { Time.now.strftime("%d/%m/%Y") }
 
   it "is initialized with an amount" do
     expect(subject.amount).to eq 100
+  end
+
+  it "is initialized with the current date" do
+    expect(subject.date).to eq current_date
   end
 end

2e12b8e47cab07691cf08a156bedf299e035fbe9
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-06-01 22:10:41 +0100

    Nil reponse dependent on events

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -16,16 +16,39 @@ RSpec.describe Transaction do
   end
 
   describe "#credit" do
-    it "returns the credit amount" do
-      subject = described_class.new({ credit: 1000 })
-      expect(subject.credit).to eq 1000
+    context "depositing funds" do
+      let(:subject) { described_class.new({ credit: 1000 }) }
+
+      it "returns the credit amount" do
+        expect(subject.credit).to eq 1000
+      end
+    end
+
+    context "withdrawing funds" do
+      let(:subject) { described_class.new({ debit: 500 }) }
+      
+      it "returns nil" do
+        expect(subject.credit).to be_nil
+      end
     end
   end
 
   describe "#debit" do
-    it "returns the debit amount" do
-      subject = described_class.new({ debit: 500 })
-      expect(subject.debit).to eq 500
+    context "depositing funds" do
+      let(:subject) { described_class.new({ credit: 1000 }) }
+
+      it "returns nil" do
+        expect(subject.debit).to be_nil
+      end
+    end
+
+    context "withdrawing funds" do
+      let(:subject) { described_class.new({ debit: 500 }) }
+      
+      it "returns the debit amount" do
+        expect(subject.debit).to eq 500
+      end
     end
   end
+
 end
